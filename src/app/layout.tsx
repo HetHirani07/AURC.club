9982f8d80bd2a9fa5e611392ff5ab3796f221a43
HetHirani07 <hethirani75@outlook.com>
2024-12-31 19:02:32 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,14 +9,14 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="en">
       <body>
         {children}
       </body>
     </html>
-  );
+  )
 }
