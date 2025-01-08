8b2c24278daacf8f4630307b8e12168b661e6853
HetHirani07 <hethirani75@outlook.com>
2025-01-08 10:53:08 +0530

    teams

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -1,13 +1,33 @@
 'use client';
 import ReactLenis from 'lenis/react';
-import { useLayoutEffect } from 'react';
+import {Poppins} from 'next/font/google';
+import { useLayoutEffect, useState, useRef} from 'react';
 import { gsap} from 'gsap';
 import {ScrollTrigger} from 'gsap/ScrollTrigger';
 import Link from 'next/link';
 import Head from 'next/head';
 import styles from './team.module.css';
 
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
+});
+
 export default function Team(){
+  const menuRef = useRef(null);
+  const [isMenu, setIsMenu] = useState(true);
+
+  const handleNavbar = () =>{
+      setIsMenu(!isMenu);
+      const menu = menuRef.current;
+      if(menu){
+        gsap.to(menu, {
+          height: isMenu ? '15vh': '0vh',
+          duration: 0.3,
+          ease: 'power2.Out'
+        })
+      }
+  };
 
     useLayoutEffect(() => {
         gsap.registerPlugin(ScrollTrigger)
@@ -28,7 +48,7 @@ export default function Team(){
       <link rel="preconnect" href="https://fonts.gstatic.com" />
       <link href="https://fonts.googleapis.com/css2?family=Sometype+Mono:ital,wght@0,400..700;1,400..700&display=swap" rel="stylesheet" />
       </Head>
-        <main>
+        <main className={poppins.className}>
         <div style={{position: 'relative'}}>
             <nav id="nav" className={styles.navbar}>
             <div className={styles.navbarContainer}>
@@ -41,24 +61,40 @@ export default function Team(){
                 <Link href='/team' className={styles.navbarText}>Team</Link>
                 {/* <span className={styles.navbarText}>About</span> */}
               </div>
+
+              <div className={styles.navbarIcon} onClick={handleNavbar}>
+                <img src='/RectBullet.png' style={{width: 10, height: 10}} />
+                <img src='/RectBullet.png' style={{width: 10, height: 10, marginLeft: 2}} />
+                <img src='/RectBullet.png' style={{width: 10, height: 10, marginTop: 1}} />
+                <img src='/RectBullet.png' style={{width: 10, height: 10, marginLeft: 2, marginTop: 1}} />
+              </div>
+              <div ref={menuRef} className={styles.navbarMenu}>
+                <Link href='/#home' className={styles.navbarText} style={{marginLeft: 10, marginTop: 15}}>Home</Link>
+                <Link href='/#events' className={styles.navbarText} style={{marginLeft: 10, marginTop: 8}}>Events</Link>
+                <Link href='/team' className={styles.navbarText} style={{marginLeft: 10, marginTop: 8}}>Team</Link>
+              </div>
             </div>
             </nav>
             
             <section className={styles.main}>
             <div>
             <img src="/drone.png" id="drone" className={styles.drone}></img>
-            <div className={styles.droneLanding} style={{marginLeft: '.5%', marginTop: '40.7%'}}></div>
-            <div className={styles.droneLanding} style={{marginLeft: '.5%', marginTop: '80.7%'}}></div>
-            </div>
+            <div className={styles.droneLanding1}></div>
+            <div className={styles.droneLanding2}></div>
+          </div>
 
             <div className={styles.hero}>
-                <div style={{display: 'flex', alignItems: 'center', marginTop: 50, marginLeft: 40}}>
+                <div className={styles.titleContainer}>
                     <img src='/RectBullet.png' className={styles.bullet}></img>
                     <span className={styles.title}>Team</span>
                 </div>
 
                 <div className={styles.cardBox}>
-                    <div className={styles.card}></div>
+                    <div className={styles.card}>
+                      <img src='/hetProfile.png' className={styles.imgProfile} alt='profileImage'></img>
+                      <span className={styles.name}>Het Hirani</span>
+                      <span className={styles.subName}>Trainer</span>
+                    </div>
                     <div className={styles.card}></div>
                     <div className={styles.card}></div>
                     <div className={styles.card}></div>
@@ -74,15 +110,15 @@ export default function Team(){
             </div>
             </section>
             <footer className={styles.footer}>
-                    <div style={{display: 'flex', flexDirection: 'column', marginLeft: '10%', marginTop: '2%'}}>
-                    <span className={styles.footerTitle}>AURC</span>
-                    <span className={styles.footerSubTitle}>Learn | Connect | Evolve</span>
-                    <span className={styles.footerConnect}>Connect With Us</span>
-                    </div>
-                    <div>
-                      <img src='/AURC_logo.png' className={styles.footerImg}></img>
-                    </div>
-            </footer>
+          <div style={{display: 'flex', flexDirection: 'column'}}>
+          <span className={styles.footerTitle}>AURC</span>
+          <span className={styles.footerSubTitle}>Learn | Connect | Evolve</span>
+          <span className={styles.footerConnect}>Connect With Us</span>
+          </div>
+          <div>
+            <img src='/AURC_logo.png' className={styles.footerImg}></img>
+          </div>
+        </footer>
         </div>
         </main>
         </ReactLenis>
