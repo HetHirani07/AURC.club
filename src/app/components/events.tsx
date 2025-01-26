8ad5fe6352321b5e84edd89f02903c606af8ac20
HetHirani07 <hethirani75@outlook.com>
2025-01-26 11:38:25 +0530

    event

--- src/app/components/events.tsx
+++ src/app/components/events.tsx
@@ -9,13 +9,13 @@ export default function Events(){
                 <img src='/RectBullet.png' className={styles.bullet}></img>
                 <span className={styles.title}>Events</span>
             </div>
-            {/* <p className={styles.subTitle}>Join events to learn and grow with better connections.</p> */}
-            <div className={styles.card}>
+            <p className={styles.subTitle}>Join events to learn and grow with better connections. Stay connected for upcoming events</p> 
+            {/* <div className={styles.card}>
                 <img src='/workshopBG.png' className={styles.cardImg}></img>
                 <span className={styles.eventName}>Robotics Workshop S16</span>
                 <span className={styles.eventDate}>Date: 24th Jan, 2025</span>
                 <Link href='/event'><img src='/cardToggle.png' className={styles.toggleImg}></img></Link>
-            </div>
+            </div> */}
         </div>
     )
 }
